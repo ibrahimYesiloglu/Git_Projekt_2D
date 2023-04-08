5990878e4a37cf098bf03d0af44c5ce5d82ef827
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-04-08 11:35:17 +0200

    initial comit 1...

---
+++ app/src/main/java/com/example/git_projekt_2/Food.java
@@ -0,0 +1,5 @@
+package com.example.git_projekt_2;
+
+public class Food {
+
+}
---
+++ app/src/main/java/com/example/git_projekt_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.git_projekt_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("Selam")
+    }
+}
